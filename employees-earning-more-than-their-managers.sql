fcd9c5e36398f973f3bc01dda15af958d3a1c2c9
Ataraxist <cantico@gmail.com>
2025-09-03 10:27:41 -0700

    Todays POTD

--- employees-earning-more-than-their-managers.sql
+++ employees-earning-more-than-their-managers.sql
@@ -49,14 +49,13 @@
 
 -- Write your MySQL query statement below
 
-SELECT name AS Employee
+SELECT name AS 'Employee'
   FROM Employee AS e1
   WHERE salary > (
       SELECT salary
       FROM Employee AS e2
       WHERE e2.id = e1.managerId
   );
-
 
 -- SELECT e.name AS Employee
 -- FROM Employee AS e
